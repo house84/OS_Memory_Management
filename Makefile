db10b50946e2451ed9cf330ec05281ebb860e2e8
Nick House <nh6gy@umsystem.edu>
2021-04-29 13:52:56 -0500

    Rebase

---
+++ Makefile
@@ -0,0 +1,35 @@
+CC = gcc
+CFLAGS = -g 
+LDLIBS = -lm -lpthread -lrt
+LIBPATH = -L . -l 
+
+TARGET1 = oss
+OBJ1 = oss.o
+
+TARGET2 = user_proc
+OBJ2 = user.o
+
+TARGETLIB = libsharedFunc.a
+LIBOBJ = sharedFunc.o
+LIBC = sharedFunc.c
+
+HEADERS = headers.h shared.h oss.h user.h sharedFunc.h
+
+.SUFFIXES: .c .o
+
+ALL: $(TARGETLIB) $(TARGET1) $(TARGET2)
+
+$(TARGET1): $(OBJ1)
+	$(CC) $(CFLAGS) -o $(TARGET1) $(OBJ1) $(TARGETLIB) $(LDLIBS)
+
+$(TARGET2): $(OBJ2) 
+	$(CC) $(CFLAGS) -o $(TARGET2) $(OBJ2) $(TARGETLIB) $(LIDLIBS)
+
+$(TARGETLIB): $(LIBOBJ) $(HEADERS)
+	ar rs $@ $^
+
+$(LIBOBJ): $(LIBC)
+	$(CC) -c $(LIBC)
+
+clean: 
+	rm -f $(TARGET1) $(TARGET2) $(TARGETLIB) *.o
---
+++ headers.h
@@ -0,0 +1,36 @@
+/*
+ * Author: Nick House
+ * Project: Memory Management
+ * Course: CS-4760 Operating Systems, Spring 2021
+ * File Name: headers.h
+ */
+
+#ifndef HEADERS_H
+#define HEADERS_H
+
+#include <semaphore.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <limits.h>
+#include <stdarg.h>
+#include <errno.h>
+#include <math.h>
+#include <stdbool.h>
+#include <signal.h>
+#include <time.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <ctype.h>
+#include <sys/ipc.h>
+#include <sys/sem.h>
+#include <sys/shm.h>
+#include <sys/msg.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/time.h>
+#include <sys/queue.h>
+#include <sys/wait.h>
+#include "shared.h" 
+
+#endif
